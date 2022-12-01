58b8dad454b9326b85b6a2f6af11937233d8326e
pjastr <24464854+pjastr@users.noreply.github.com>
2022-12-01 13:56:22 +0100

    Initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public TMP_Text text;
+    public TMP_InputField input;
+    Save save = new Save();
+    
+    public void setA1()
+    {
+        PlayerPrefs.SetString("A1", input.text);
+    }
+
+    public void getA1()
+    {
+        string temp = PlayerPrefs.GetString("A1");
+        text.SetText(temp);
+    }
+
+    public void SaveData()
+    {
+        string saveJson = JsonUtility.ToJson(save);
+        PlayerPrefs.SetString("save1", saveJson);
+        PlayerPrefs.Save();
+    }
+
+    public void LoadData()
+    {
+        if (PlayerPrefs.HasKey("save1"))
+        {
+            string saveJson = PlayerPrefs.GetString("save1");
+            save = JsonUtility.FromJson<Save>(saveJson);
+            text.SetText(save.ToString());
+        }
+    }
+}
+
+
+public class Save
+{
+    public int number;
+    public Vector3 vector3;
+    public bool isBool;
+
+    public override string ToString()
+    {
+        return number.ToString() + Environment.NewLine+
+            vector3.ToString() + Environment.NewLine + isBool.ToString();
+    }
+}
